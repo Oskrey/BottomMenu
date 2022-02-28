ef83bc31dcd4d70635478bb8d1873376eea26360
Никита <pedichn@gmail.com>
2022-03-01 01:44:22 +0300

    a

--- app/src/main/java/com/example/bottommenu/History.java
+++ app/src/main/java/com/example/bottommenu/History.java
@@ -19,25 +19,25 @@ public class History extends AppCompatActivity {
         BottomNavigationView nav = findViewById(R.id.nav);
         nav.setSelectedItemId(R.id.history);
         nav.setOnItemSelectedListener(new
-      NavigationBarView.OnItemSelectedListener() {
-          @Override
-          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-              Intent i = null;
-              switch (item.getItemId()){
-                  case R.id.history:
-                      i = new Intent(getApplicationContext(), History.class);
-                      break;
-                  case R.id.home:
-                      i = new Intent(getApplicationContext(),
-                              MainActivity.class);
-                      break;
-              }
-              if(i != null) {
-                  i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                  startActivity(i);
-              }
-              return true;
-          }
-      });
+                                              NavigationBarView.OnItemSelectedListener() {
+                                                  @Override
+                                                  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                                                      Intent i = null;
+                                                      switch (item.getItemId()){
+                                                          case R.id.history:
+                                                              i = new Intent(getApplicationContext(), History.class);
+                                                              break;
+                                                          case R.id.home:
+                                                              i = new Intent(getApplicationContext(),
+                                                                      MainActivity.class);
+                                                              break;
+                                                      }
+                                                      if(i != null) {
+                                                          i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                          startActivity(i);
+                                                      }
+                                                      return true;
+                                                  }
+                                              });
     }
 }
--- app/src/main/java/com/example/bottommenu/MainActivity.java
+++ app/src/main/java/com/example/bottommenu/MainActivity.java
@@ -19,26 +19,26 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView nav = findViewById(R.id.nav);
         nav.setSelectedItemId(R.id.home);
         nav.setOnItemSelectedListener(new
-          NavigationBarView.OnItemSelectedListener() {
-              @Override
-              public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                  Intent i = null;
-                  switch (item.getItemId()){
-                      case R.id.history:
-                          i = new Intent(getApplicationContext(), History.class);
-                          break;
-                      case R.id.home:
-                          i = new Intent(getApplicationContext(),
-                                  MainActivity.class);
-                          break;
-                  }
-                  if(i != null) {
-                      i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                      startActivity(i);
-                  }
-                  return true;
-              }
-          });
+                                              NavigationBarView.OnItemSelectedListener() {
+                                                  @Override
+                                                  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                                                      Intent i = null;
+                                                      switch (item.getItemId()){
+                                                          case R.id.history:
+                                                              i = new Intent(getApplicationContext(), History.class);
+                                                              break;
+                                                          case R.id.home:
+                                                              i = new Intent(getApplicationContext(),
+                                                                      MainActivity.class);
+                                                              break;
+                                                      }
+                                                      if(i != null) {
+                                                          i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                          startActivity(i);
+                                                      }
+                                                      return true;
+                                                  }
+                                              });
 
     }
 }
