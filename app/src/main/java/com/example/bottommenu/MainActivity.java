ab0a16fff7eff0aed7279be44ddc0ac3f419fa14
Никита <pedichn@gmail.com>
2022-02-28 23:35:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bottommenu/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.bottommenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView nav = findViewById(R.id.nav);
+        nav.setSelectedItemId(R.id.home);
+        nav.setOnItemSelectedListener(new
+          NavigationBarView.OnItemSelectedListener() {
+              @Override
+              public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                  Intent i = null;
+                  switch (item.getItemId()){
+                      case R.id.history:
+                          i = new Intent(getApplicationContext(), History.class);
+                          break;
+                      case R.id.home:
+                          i = new Intent(getApplicationContext(),
+                                  MainActivity.class);
+                          break;
+                  }
+                  if(i != null) {
+                      i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                      startActivity(i);
+                  }
+                  return true;
+              }
+          });
+
+    }
+}
